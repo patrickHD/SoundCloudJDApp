d0f917baa75f060f8adc1a6834dc138018f45378
patrickHD <patrickhdaniel@outlook.com>
2017-07-22 22:43:19 -0500

    Second Commit

---
+++ WindowsFormsTest/WindowsFormsTest/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openButton = new System.Windows.Forms.Button();
+			this.outText = new System.Windows.Forms.TextBox();
+			this.dispRadio = new System.Windows.Forms.RadioButton();
+			this.tagRadio = new System.Windows.Forms.RadioButton();
+			this.SuspendLayout();
+			// 
+			// openButton
+			// 
+			this.openButton.Location = new System.Drawing.Point(12, 12);
+			this.openButton.Name = "openButton";
+			this.openButton.Size = new System.Drawing.Size(207, 41);
+			this.openButton.TabIndex = 0;
+			this.openButton.Text = "Open Folder";
+			this.openButton.UseVisualStyleBackColor = true;
+			this.openButton.Click += new System.EventHandler(this.openButton_Click);
+			// 
+			// outText
+			// 
+			this.outText.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.outText.Location = new System.Drawing.Point(0, 60);
+			this.outText.Multiline = true;
+			this.outText.Name = "outText";
+			this.outText.Size = new System.Drawing.Size(322, 420);
+			this.outText.TabIndex = 1;
+			// 
+			// dispRadio
+			// 
+			this.dispRadio.AutoSize = true;
+			this.dispRadio.Checked = true;
+			this.dispRadio.Location = new System.Drawing.Point(225, 12);
+			this.dispRadio.Name = "dispRadio";
+			this.dispRadio.Size = new System.Drawing.Size(89, 17);
+			this.dispRadio.TabIndex = 2;
+			this.dispRadio.TabStop = true;
+			this.dispRadio.Text = "Display Mode";
+			this.dispRadio.UseVisualStyleBackColor = true;
+			// 
+			// tagRadio
+			// 
+			this.tagRadio.AutoSize = true;
+			this.tagRadio.Location = new System.Drawing.Point(225, 36);
+			this.tagRadio.Name = "tagRadio";
+			this.tagRadio.Size = new System.Drawing.Size(74, 17);
+			this.tagRadio.TabIndex = 3;
+			this.tagRadio.Text = "Tag Mode";
+			this.tagRadio.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(322, 480);
+			this.Controls.Add(this.tagRadio);
+			this.Controls.Add(this.dispRadio);
+			this.Controls.Add(this.outText);
+			this.Controls.Add(this.openButton);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button openButton;
+		private System.Windows.Forms.TextBox outText;
+		private System.Windows.Forms.RadioButton dispRadio;
+		private System.Windows.Forms.RadioButton tagRadio;
+	}
+}
+
---
+++ WindowsFormsTest/WindowsFormsTest/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace WindowsFormsTest
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void openButton_Click(object sender, EventArgs e)
+		{
+			CommonOpenFileDialog dialog = new CommonOpenFileDialog()
+			{
+				InitialDirectory = "D:\\Download\\",
+				IsFolderPicker = true
+			};
+			string[] files;
+			string[] artists = { "" };
+			bool good = false;
+			TagLib.File targetFile;
+			TagLib.Id3v2.AttachedPictureFrame pic;
+			if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+			{
+				good = true;
+			}
+
+			if (good == true)
+			{
+				files = Directory.GetFiles(dialog.FileName);
+				if (dispRadio.Checked == true)
+				{
+					for (int i = 0; i < files.Length; i += 2)
+					{
+						outText.AppendText(files[i].Split('-').Last().Split('.')[0].Trim(' '));
+						outText.AppendText(Environment.NewLine);
+					}
+				}
+				else
+				{
+					for (int i = 0; i < files.Length - 1; i++)
+					{
+						if (files[i].Split('\\').Last().Split('_')[0] == files[i + 1].Split('\\').Last().Split('_')[0] && files[i].Split('.').Last() == "mp3")
+						{
+							pic = new TagLib.Id3v2.AttachedPictureFrame()
+							{
+								TextEncoding = TagLib.StringType.Latin1,
+								MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg,
+								Type = TagLib.PictureType.FrontCover,
+								Data = TagLib.ByteVector.FromPath(files[i + 1])
+							};
+
+							targetFile = TagLib.File.Create(files[i]);
+							targetFile.Tag.Pictures = new TagLib.IPicture[1] { pic };
+							targetFile.Tag.Album = files[i].Split('\\').Last().Split('_')[0];
+							artists[0] = files[i].Split('-').Last().Split('.')[0].Trim(' ');
+							targetFile.Tag.AlbumArtists = artists;
+							targetFile.Save();
+						}
+					}
+					outText.AppendText("Done");
+					outText.AppendText(Environment.NewLine);
+				}
+				
+			}
+		}
+	}
+
+
+
+}
