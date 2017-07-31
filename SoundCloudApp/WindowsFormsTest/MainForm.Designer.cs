72d6eaeaebb56c9fa5c05f84065423d5250fd224
patrickHD <patrickhdaniel@outlook.com>
2017-07-31 01:34:52 -0500

    Added some things that allow you to add art to files w/o them bring in the JD2 format. It's so un-user friendly I confused myself. Please don't use this program.

--- SoundCloudApp/WindowsFormsTest/MainForm.Designer.cs
+++ SoundCloudApp/WindowsFormsTest/MainForm.Designer.cs
@@ -28,66 +28,130 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.openButton = new System.Windows.Forms.Button();
-			this.outText = new System.Windows.Forms.TextBox();
-			this.dispRadio = new System.Windows.Forms.RadioButton();
-			this.tagRadio = new System.Windows.Forms.RadioButton();
-			this.SuspendLayout();
-			// 
-			// openButton
-			// 
-			this.openButton.Location = new System.Drawing.Point(12, 12);
-			this.openButton.Name = "openButton";
-			this.openButton.Size = new System.Drawing.Size(207, 41);
-			this.openButton.TabIndex = 0;
-			this.openButton.Text = "Open Folder";
-			this.openButton.UseVisualStyleBackColor = true;
-			this.openButton.Click += new System.EventHandler(this.openButton_Click);
-			// 
-			// outText
-			// 
-			this.outText.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.outText.Location = new System.Drawing.Point(0, 60);
-			this.outText.Multiline = true;
-			this.outText.Name = "outText";
-			this.outText.Size = new System.Drawing.Size(322, 420);
-			this.outText.TabIndex = 1;
-			// 
-			// dispRadio
-			// 
-			this.dispRadio.AutoSize = true;
-			this.dispRadio.Checked = true;
-			this.dispRadio.Location = new System.Drawing.Point(225, 12);
-			this.dispRadio.Name = "dispRadio";
-			this.dispRadio.Size = new System.Drawing.Size(89, 17);
-			this.dispRadio.TabIndex = 2;
-			this.dispRadio.TabStop = true;
-			this.dispRadio.Text = "Display Mode";
-			this.dispRadio.UseVisualStyleBackColor = true;
-			// 
-			// tagRadio
-			// 
-			this.tagRadio.AutoSize = true;
-			this.tagRadio.Location = new System.Drawing.Point(225, 36);
-			this.tagRadio.Name = "tagRadio";
-			this.tagRadio.Size = new System.Drawing.Size(74, 17);
-			this.tagRadio.TabIndex = 3;
-			this.tagRadio.Text = "Tag Mode";
-			this.tagRadio.UseVisualStyleBackColor = true;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(322, 480);
-			this.Controls.Add(this.tagRadio);
-			this.Controls.Add(this.dispRadio);
-			this.Controls.Add(this.outText);
-			this.Controls.Add(this.openButton);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.openButton = new System.Windows.Forms.Button();
+            this.outText = new System.Windows.Forms.TextBox();
+            this.dispRadio = new System.Windows.Forms.RadioButton();
+            this.tagRadio = new System.Windows.Forms.RadioButton();
+            this.folderRadio = new System.Windows.Forms.RadioButton();
+            this.fileRadio = new System.Windows.Forms.RadioButton();
+            this.modeGroup = new System.Windows.Forms.GroupBox();
+            this.artCheck = new System.Windows.Forms.CheckBox();
+            this.artButton = new System.Windows.Forms.Button();
+            this.modeGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(100, 36);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // outText
+            // 
+            this.outText.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.outText.Location = new System.Drawing.Point(0, 94);
+            this.outText.Multiline = true;
+            this.outText.Name = "outText";
+            this.outText.Size = new System.Drawing.Size(322, 369);
+            this.outText.TabIndex = 1;
+            // 
+            // dispRadio
+            // 
+            this.dispRadio.AutoSize = true;
+            this.dispRadio.Location = new System.Drawing.Point(6, 19);
+            this.dispRadio.Name = "dispRadio";
+            this.dispRadio.Size = new System.Drawing.Size(89, 17);
+            this.dispRadio.TabIndex = 2;
+            this.dispRadio.Text = "Display Mode";
+            this.dispRadio.UseVisualStyleBackColor = true;
+            // 
+            // tagRadio
+            // 
+            this.tagRadio.AutoSize = true;
+            this.tagRadio.Checked = true;
+            this.tagRadio.Location = new System.Drawing.Point(6, 36);
+            this.tagRadio.Name = "tagRadio";
+            this.tagRadio.Size = new System.Drawing.Size(74, 17);
+            this.tagRadio.TabIndex = 3;
+            this.tagRadio.TabStop = true;
+            this.tagRadio.Text = "Tag Mode";
+            this.tagRadio.UseVisualStyleBackColor = true;
+            // 
+            // folderRadio
+            // 
+            this.folderRadio.AutoSize = true;
+            this.folderRadio.Checked = true;
+            this.folderRadio.Location = new System.Drawing.Point(12, 54);
+            this.folderRadio.Name = "folderRadio";
+            this.folderRadio.Size = new System.Drawing.Size(87, 17);
+            this.folderRadio.TabIndex = 4;
+            this.folderRadio.TabStop = true;
+            this.folderRadio.Text = "Folder Select";
+            this.folderRadio.UseVisualStyleBackColor = true;
+            // 
+            // fileRadio
+            // 
+            this.fileRadio.AutoSize = true;
+            this.fileRadio.Location = new System.Drawing.Point(118, 54);
+            this.fileRadio.Name = "fileRadio";
+            this.fileRadio.Size = new System.Drawing.Size(85, 17);
+            this.fileRadio.TabIndex = 5;
+            this.fileRadio.Text = "File(s) Select";
+            this.fileRadio.UseVisualStyleBackColor = true;
+            // 
+            // modeGroup
+            // 
+            this.modeGroup.Controls.Add(this.artCheck);
+            this.modeGroup.Controls.Add(this.dispRadio);
+            this.modeGroup.Controls.Add(this.tagRadio);
+            this.modeGroup.Location = new System.Drawing.Point(225, 12);
+            this.modeGroup.Name = "modeGroup";
+            this.modeGroup.Size = new System.Drawing.Size(97, 82);
+            this.modeGroup.TabIndex = 6;
+            this.modeGroup.TabStop = false;
+            this.modeGroup.Text = "Mode";
+            // 
+            // artCheck
+            // 
+            this.artCheck.AutoSize = true;
+            this.artCheck.Location = new System.Drawing.Point(6, 59);
+            this.artCheck.Name = "artCheck";
+            this.artCheck.Size = new System.Drawing.Size(63, 17);
+            this.artCheck.TabIndex = 7;
+            this.artCheck.Text = "Art Only";
+            this.artCheck.UseVisualStyleBackColor = true;
+            // 
+            // artButton
+            // 
+            this.artButton.Location = new System.Drawing.Point(118, 12);
+            this.artButton.Name = "artButton";
+            this.artButton.Size = new System.Drawing.Size(100, 36);
+            this.artButton.TabIndex = 7;
+            this.artButton.Text = "Select Art";
+            this.artButton.UseVisualStyleBackColor = true;
+            this.artButton.Click += new System.EventHandler(this.artButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 463);
+            this.Controls.Add(this.artButton);
+            this.Controls.Add(this.modeGroup);
+            this.Controls.Add(this.fileRadio);
+            this.Controls.Add(this.folderRadio);
+            this.Controls.Add(this.outText);
+            this.Controls.Add(this.openButton);
+            this.Name = "MainForm";
+            this.Text = "Tagging App";
+            this.modeGroup.ResumeLayout(false);
+            this.modeGroup.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -97,6 +161,11 @@
 		private System.Windows.Forms.TextBox outText;
 		private System.Windows.Forms.RadioButton dispRadio;
 		private System.Windows.Forms.RadioButton tagRadio;
-	}
+        private System.Windows.Forms.RadioButton folderRadio;
+        private System.Windows.Forms.RadioButton fileRadio;
+        private System.Windows.Forms.GroupBox modeGroup;
+        private System.Windows.Forms.CheckBox artCheck;
+        private System.Windows.Forms.Button artButton;
+    }
 }
 
--- SoundCloudApp/WindowsFormsTest/MainForm.cs
+++ SoundCloudApp/WindowsFormsTest/MainForm.cs
@@ -12,64 +12,136 @@ namespace WindowsFormsTest
 		{
 			InitializeComponent();
 		}
-
+        public string art;
+        public bool artGood = false;
 		private void openButton_Click(object sender, EventArgs e)
 		{
 			CommonOpenFileDialog dialog = new CommonOpenFileDialog()
 			{
-				InitialDirectory = "D:\\Download\\",
-				IsFolderPicker = true
+				InitialDirectory = "D:\\Download\\"
 			};
+            if(fileRadio.Checked == true){
+                dialog.Multiselect = true;
+            }
+            else{
+                dialog.IsFolderPicker = true;
+            }
 			string[] files;
 			string[] artists = { "" };
 			bool good = false;
 			TagLib.File targetFile;
 			TagLib.Id3v2.AttachedPictureFrame pic;
-			if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
 			{
 				good = true;
 			}
+            if (dialog.IsFolderPicker == true)
+            {
+                files = Directory.GetFiles(dialog.FileName);
+            }
+            else
+            {
+                files = dialog.FileNames.ToArray();
+            }
 
-			if (good == true)
-			{
-				files = Directory.GetFiles(dialog.FileName);
-				if (dispRadio.Checked == true)
-				{
-					for (int i = 0; i < files.Length; i += 2)
-					{
-						outText.AppendText(files[i].Split('-').Last().Split('.')[0].Trim(' '));
-						outText.AppendText(Environment.NewLine);
-					}
-				}
-				else
-				{
-					for (int i = 0; i < files.Length - 1; i++)
-					{
-						if (files[i].Split('\\').Last().Split('_')[0] == files[i + 1].Split('\\').Last().Split('_')[0] && files[i].Split('.').Last() == "mp3")
-						{
-							pic = new TagLib.Id3v2.AttachedPictureFrame()
-							{
-								TextEncoding = TagLib.StringType.Latin1,
-								MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg,
-								Type = TagLib.PictureType.FrontCover,
-								Data = TagLib.ByteVector.FromPath(files[i + 1])
-							};
+            if(good == true)
+            {
+                if (dispRadio.Checked == true)
+                {
+                    for (int i = 0; i < files.Length; i += 2)
+                    {
+                        outText.AppendText(files[i].Split('-').Last().Split('.')[0].Trim(' '));
+                        outText.AppendText(Environment.NewLine);
+                    }
+                    outText.AppendText("Done");
+                    outText.AppendText(Environment.NewLine);
+                }
+                else
+                {
+                    if (folderRadio.Checked == true)
+                    {
+                        for (int i = 0; i < files.Length - 1; i++)
+                        {
+                            if (files[i].Split('\\').Last().Split('_')[0] == files[i + 1].Split('\\').Last().Split('_')[0] && files[i].Split('.').Last() == "mp3" || files[i].Split('.').Last() == "wav")
+                            {
+                                pic = new TagLib.Id3v2.AttachedPictureFrame()
+                                {
+                                    TextEncoding = TagLib.StringType.Latin1,
+                                    MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg,
+                                    Type = TagLib.PictureType.FrontCover,
+                                    Data = TagLib.ByteVector.FromPath(files[i + 1])
+                                };
 
-							targetFile = TagLib.File.Create(files[i]);
-							targetFile.Tag.Pictures = new TagLib.IPicture[1] { pic };
-							targetFile.Tag.Album = files[i].Split('\\').Last().Split('_')[0];
-							artists[0] = files[i].Split('-').Last().Split('.')[0].Trim(' ');
-							targetFile.Tag.AlbumArtists = artists;
-							targetFile.Save();
-						}
-					}
-					outText.AppendText("Done");
-					outText.AppendText(Environment.NewLine);
-				}
-				
-			}
+                                targetFile = TagLib.File.Create(files[i]);
+                                targetFile.Tag.Pictures = new TagLib.IPicture[1] { pic };
+                                targetFile.Tag.Album = files[i].Split('\\').Last().Split('_')[0];
+                                artists[0] = files[i].Split('-').Last().Split('.')[0].Trim(' ');
+                                targetFile.Tag.AlbumArtists = artists;
+                                targetFile.Save();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (artGood == true)
+                        {
+                            for (int i = 0; i < files.Length; i++)
+                            {
+                                pic = new TagLib.Id3v2.AttachedPictureFrame()
+                                {
+                                    TextEncoding = TagLib.StringType.Latin1,
+                                    MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg,
+                                    Type = TagLib.PictureType.FrontCover,
+                                    Data = TagLib.ByteVector.FromPath(art)
+                                };
+                                targetFile = TagLib.File.Create(files[i]);
+                                targetFile.Tag.Pictures = new TagLib.IPicture[1] { pic };
+                                targetFile.Save();
+                            }
+                        }
+                        else
+                        {
+                            outText.AppendText("Need art");
+                            outText.AppendText(Environment.NewLine);
+                        }
+
+                    }
+                    outText.AppendText("Done");
+                    outText.AppendText(Environment.NewLine);
+                }
+            }
+            else
+            {
+                outText.AppendText("Not good");
+                outText.AppendText(Environment.NewLine);
+            }
+            
 		}
-	}
+
+        private void artButton_Click(object sender, EventArgs e)
+        {
+            artGood = true;
+            folderRadio.Checked = false;
+            fileRadio.Checked = true;
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog()
+            {
+                InitialDirectory = "D:\\Download\\",
+                Multiselect = false
+            };
+            if(dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                art = dialog.FileName;
+                outText.AppendText(art);
+                outText.AppendText(Environment.NewLine);
+            }
+            else
+            {
+                outText.AppendText("Bad");
+                outText.AppendText(Environment.NewLine);
+            }
+        }
+    }
 
 
 
